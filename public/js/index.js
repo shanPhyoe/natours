74632526650bca3be5ad155dd04ebe20792bc337
shan-phyoe <shan.phyoe97@gmail.com>
2022-06-12 21:00:26 +0630

    fixed bug

--- public/js/index.js
+++ public/js/index.js
@@ -72,5 +72,5 @@ document.querySelector('#book-tour')?.addEventListener('click', async e => {
     await bookTour(tourId);
 });
 
-const alertMessage = document.quertSelector('body').dataset.alert;
+const alertMessage = document.querySelector('body').dataset.alert;
 if (alertMessage) showAlert('success', alertMessage, 15);
